0ecf80aea5b33a86478320134a0a02167bd08196
olenka159 <olenka159@mail.ru>
2022-05-29 23:55:34 +0500

    ДЗ № 7

---
+++ lesson7/src/Cat.java
@@ -0,0 +1,21 @@
+class Cat {
+    private String name;
+    private int appetite;
+    private boolean hungry;
+
+    Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.hungry = true;
+    }
+
+    void info() {
+        String isHungry = !hungry ? "сыт" : "голоден";
+        System.out.println(name + ": " + isHungry);
+    }
+
+    void eat(Animal animal) {
+        if (hungry && animal.decreaseFood(appetite))
+            hungry = false;
+    }
+}
